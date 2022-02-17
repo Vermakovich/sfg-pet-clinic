2f84836a87a065a9deb9a0981d52a19e86d0f4ff
Vitali <v.yermakovich@edoktor24.pl>
2022-02-17 14:23:28 +0100

    add POJO's

---
+++ src/main/java/com/vermakovich/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.vermakovich.sfgpetclinic.model;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/com/vermakovich/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.vermakovich.sfgpetclinic.model;
+
+public class Vet extends Person{
+}
