7e250763ca7a97c6a20b2bb98f15c8745526e4db
Vitali <v.yermakovich@edoktor24.pl>
2022-02-21 12:04:44 +0100

    refactor to common crud interface

--- pet-clinic-data/src/main/java/com/vermakovich/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/vermakovich/sfgpetclinic/services/OwnerService.java
@@ -2,15 +2,7 @@ package com.vermakovich.sfgpetclinic.services;
 
 import com.vermakovich.sfgpetclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
-
-    Owner findById(Long id);
+public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
-
-    Set<Owner> findAll();
-
-    Owner save(Owner owner);
 }
--- pet-clinic-data/src/main/java/com/vermakovich/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/vermakovich/sfgpetclinic/services/VetService.java
@@ -2,13 +2,5 @@ package com.vermakovich.sfgpetclinic.services;
 
 import com.vermakovich.sfgpetclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Set<Vet> findAll();
-
-    Vet save(Vet vet);
+public interface VetService extends CrudService<Vet, Long>{
 }
