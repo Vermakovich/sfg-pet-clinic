be84c02a2d9f857b00c2ac99d6b134553f0b70b1
Vitali <v.yermakovich@edoktor24.pl>
2022-03-03 16:10:04 +0100

    add owners list and change controller

--- pet-clinic-web/src/main/java/com/vermakovich/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/vermakovich/sfgpetclinic/controllers/OwnerController.java
@@ -1,14 +1,24 @@
 package com.vermakovich.sfgpetclinic.controllers;
 
+import com.vermakovich.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/index"})
-    public String getOwners() {
+    public String getOwners(Model model) {
+
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
