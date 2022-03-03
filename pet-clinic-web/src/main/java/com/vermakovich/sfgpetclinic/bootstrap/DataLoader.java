dbbf06da272230fad9bf98c11960344a8008b048
Vitali <v.yermakovich@edoktor24.pl>
2022-03-03 12:18:37 +0100

    create bootstrap for data

---
+++ pet-clinic-web/src/main/java/com/vermakovich/sfgpetclinic/bootstrap/DataLoader.java
@@ -0,0 +1,52 @@
+package com.vermakovich.sfgpetclinic.bootstrap;
+
+import com.vermakovich.sfgpetclinic.model.Owner;
+import com.vermakovich.sfgpetclinic.model.Vet;
+import com.vermakovich.sfgpetclinic.services.map.OwnerServiceMap;
+import com.vermakovich.sfgpetclinic.services.map.VetServiceMap;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+
+    private final OwnerServiceMap ownerServiceMap;
+    private final VetServiceMap vetServiceMap;
+
+    public DataLoader() {
+        ownerServiceMap = new OwnerServiceMap();
+        vetServiceMap = new VetServiceMap();
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Owner owner1 = new Owner();
+        owner1.setId(1L);
+        owner1.setFirstName("Mat");
+        owner1.setLastName("Rives");
+        ownerServiceMap.save(owner1);
+
+        Owner owner2 = new Owner();
+        owner2.setId(2L);
+        owner2.setFirstName("Rob");
+        owner2.setLastName("Pattins");
+        ownerServiceMap.save(owner2);
+
+        System.out.println("Owners loaded....");
+
+        Vet vet1 = new Vet();
+        vet1.setId(1L);
+        vet1.setFirstName("Colin");
+        vet1.setLastName("Farrel");
+        vetServiceMap.save(vet1);
+
+        Vet vet2 = new Vet();
+        vet2.setId(2L);
+        vet2.setFirstName("Tom");
+        vet2.setLastName("Hanks");
+        vetServiceMap.save(vet2);
+
+        System.out.println("Vets loaded....");
+    }
+}
