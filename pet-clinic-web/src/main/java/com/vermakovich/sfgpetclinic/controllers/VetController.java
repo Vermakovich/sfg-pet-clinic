2b8331caee888a02f32dc9a0d7deb45675f6bc5f
Vitali <v.yermakovich@edoktor24.pl>
2022-03-03 16:16:13 +0100

    add vets list and change controller

--- pet-clinic-web/src/main/java/com/vermakovich/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/vermakovich/sfgpetclinic/controllers/VetController.java
@@ -1,14 +1,24 @@
 package com.vermakovich.sfgpetclinic.controllers;
 
+import com.vermakovich.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("vets")
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"", "/index"})
-    public String getVets() {
+    public String getVets(Model model) {
+
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
