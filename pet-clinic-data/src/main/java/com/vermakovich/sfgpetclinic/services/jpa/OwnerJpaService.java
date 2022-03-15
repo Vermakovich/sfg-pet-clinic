140b76d4f117e2e7bb62645e2c56a087653ca13c
Vitali <v.yermakovich@edoktor24.pl>
2022-03-15 15:26:48 +0100

    add owner jpa service

---
+++ pet-clinic-data/src/main/java/com/vermakovich/sfgpetclinic/services/jpa/OwnerJpaService.java
@@ -0,0 +1,51 @@
+package com.vermakovich.sfgpetclinic.services.jpa;
+
+import com.vermakovich.sfgpetclinic.model.Owner;
+import com.vermakovich.sfgpetclinic.repositories.OwnerRepository;
+import com.vermakovich.sfgpetclinic.services.OwnerService;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class OwnerJpaService implements OwnerService {
+
+    private final OwnerRepository ownerRepository;
+
+    public OwnerJpaService(OwnerRepository ownerRepository) {
+        this.ownerRepository = ownerRepository;
+    }
+
+    @Override
+    public Set<Owner> findAll() {
+        Set<Owner> owners = new HashSet<>();
+        ownerRepository.findAll().forEach(owners::add);
+        return owners;
+    }
+
+    @Override
+    public Owner findById(Long aLong) {
+        return ownerRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return ownerRepository.save(object);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        ownerRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        ownerRepository.deleteById(aLong);
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return ownerRepository.findByLastName(lastName);
+    }
+}
