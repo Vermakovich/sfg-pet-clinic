67712321694581280ea903efe4daa201858fa98f
Vitali <v.yermakovich@edoktor24.pl>
2022-03-16 15:18:05 +0100

    add visit map service

---
+++ pet-clinic-data/src/main/java/com/vermakovich/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.vermakovich.sfgpetclinic.services;
+
+import com.vermakovich.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/vermakovich/sfgpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,40 @@
+package com.vermakovich.sfgpetclinic.services.map;
+
+import com.vermakovich.sfgpetclinic.model.Visit;
+import com.vermakovich.sfgpetclinic.services.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        super.delete(visit);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if (visit.getPet() == null || visit.getPet().getId() == null || visit.getPet().getOwner() == null
+                || visit.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid visit");
+        }
+        return super.save(visit);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
